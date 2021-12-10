9a69fe407c954335a3b1e08e234540214cab7e40
Alexandr Serebrennikov <aserebrennikov@itsumma.ru>
2021-12-10 21:02:32 +0300

    feat: separate prod and dev config

---
+++ webpack/webpack.common.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, '..', './src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, '..', './build'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          }
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(?:jpg|jpeg|png|gif|ico)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+        type: 'asset/inline'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', './src/index.html')
+    })
+  ]
+}
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,48 +1,8 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.common.js');
 
-module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, '..', './src/index.tsx'),
-  output: {
-    path: path.resolve(__dirname, '..', './build'),
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-          }
-        ]
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.(?:jpg|jpeg|png|gif|ico)$/i,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
-        type: 'asset/inline'
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '..', './src/index.html')
-    })
-  ]
+module.exports = ({ env }) => {
+  const envConfig = require(`./webpack.${env}.js`);
+  const config = merge(commonConfig, envConfig);
+  return config;
 }
